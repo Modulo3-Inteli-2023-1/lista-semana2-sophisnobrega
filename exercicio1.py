e7e07b9e6fc50619f0e26d03dfc1ff88ff4c6a93
sophisnobrega <124265768+sophisnobrega@users.noreply.github.com>
2023-08-19 17:27:20 -0300

    exercicio1_commit.py

--- exercicio1.py
+++ exercicio1.py
@@ -1,18 +1,7 @@
-#  Se achar necessario, faça import de outras bibliotecas
+# definindo a função 
+def conta_palavras_unicas(frase: str) -> int: # entra string e sai inteiro
+    return len(set(frase.split())) # retorna o tamanho da lista que se deu pelo slip da frase a cada espaço
 
-
-
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
 
 
 
