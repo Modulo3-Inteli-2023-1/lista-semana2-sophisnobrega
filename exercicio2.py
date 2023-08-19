03522773b938b8d56de36892f359e6aab73d2808
sophisnobrega <124265768+sophisnobrega@users.noreply.github.com>
2023-08-19 17:54:25 -0300

    exercicio2_commit.py

--- exercicio2.py
+++ exercicio2.py
@@ -1,18 +1,28 @@
-#  Se achar necessario, faça import de outras bibliotecas
+# função que faz a separação das letras e coloca em uma lista
+def letras_da_palavra(palavra: str) -> list:
+    letras = list(palavra)
+    return letras
 
+# input das palavras
+palavra_a = input('Escreva uma palavra: ')
+palavra_b = input('Escreva outra palavra: ')
 
+# função principal que confere se é anagrama ou não
+def is_anagram(palavra_a, palavra_b):
+    letras_a = letras_da_palavra(palavra_a)
+    letras_b = letras_da_palavra(palavra_b)
+    
+    letras_a.sort()  # ordena as letras
+    letras_b.sort()  
+    
+    return letras_a == letras_b
 
+e_anagrama = is_anagram(palavra_a, palavra_b)
+print(e_anagrama)
 
+# usei o chat gpt para me ajudar a corrijir o código
+# no vs code está funcionando tranquilamente
 
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
 
 
 
