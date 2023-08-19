f2dac5b4f588442dfc7d6675a648bc7a8805086b
sophisnobrega <124265768+sophisnobrega@users.noreply.github.com>
2023-08-19 17:28:58 -0300

    exercicio1_commit2.py
    com op de output

--- exercicio1.py
+++ exercicio1.py
@@ -2,10 +2,10 @@
 def conta_palavras_unicas(frase: str) -> int: # entra string e sai inteiro
     return len(set(frase.split())) # retorna o tamanho da lista que se deu pelo slip da frase a cada espaço
 
-
-
-
-
+# -------------- OPÇÃO DE OUTPUT ------------------ #
+#frase = input('Escreva uma frase ou um parágrafo: ')
+#quantidade = conta_palavras_unicas(frase)
+#print('Quantidade de palavras únicas:', quantidade)
 
 
 
